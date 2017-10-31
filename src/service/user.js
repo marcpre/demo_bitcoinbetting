8a5aa5eb98fb6d14b2eb1b8c4443c02b4214e0d0
Ronniel <piscoster@gmail.com>
2017-10-31 07:20:52 +0000

    adding vali admin template

--- src/service/user.js
+++ src/service/user.js
@@ -1,15 +1,15 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable("users", function (t) {
-      t.increments("id").unsigned().primary()
-      t.string("username").notNull()
-      t.string("password").notNull()
-      t.boolean("deleted").nullable()       
-      t.dateTime("createdAt").notNull()
-      t.dateTime("updatedAt").nullable()
-      t.dateTime("deletedAt").nullable()
+  return knex.schema.createTable('users', (t) => {
+    t.increments('id').unsigned().primary()
+    t.string('username').notNull()
+    t.string('password').notNull()
+    t.boolean('deleted').nullable()
+    t.dateTime('createdAt').notNull()
+    t.dateTime('updatedAt').nullable()
+    t.dateTime('deletedAt').nullable()
   })
 }
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable("users")        
+  return knex.schema.dropTable('users')
 }
